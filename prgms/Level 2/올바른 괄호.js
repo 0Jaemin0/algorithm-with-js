8616ef2f899b270dd1b483e881b1bee45229622a
0Jaemin0 <jaemin2167@naver.com>
2025-02-03 21:55:40 +0900

    solved/prgms/Level 2/올바른 괄호

---
+++ prgms/Level 2/올바른 괄호.js
@@ -0,0 +1,11 @@
+function solution(s) {
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') stack.push('(');
+    else if (stack.pop() === '(') continue;
+    else return false;
+  }
+
+  return stack.length === 0;
+}
