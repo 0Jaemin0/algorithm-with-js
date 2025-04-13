49bc8e07bdc7f27cd048df6be14700c31c461e21
0Jaemin0 <jaemin2167@naver.com>
2025-04-13 23:18:35 +0900

    solved/boj/Silver/15665

---
+++ JavaScript/boj/Silver/15665.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const [N, M] = input[0].split(' ').map(Number);
+const numbers = input[1]
+  .split(' ')
+  .map(Number)
+  .sort((a, b) => a - b);
+const result = [];
+const sequence = [];
+
+const dfs = (depth) => {
+  if (depth === M) {
+    result.push(sequence.join(' '));
+    return;
+  }
+
+  for (let i = 0; i < N; i++) {
+    sequence.push(numbers[i]);
+    dfs(depth + 1);
+    sequence.pop();
+  }
+};
+
+dfs(0);
+console.log([...new Set(result)].join('\n'));
