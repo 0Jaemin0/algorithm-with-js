1128c75a8fa6d10e588a1a5d4d44dfb4503c3803
0Jaemin0 <jaemin2167@naver.com>
2025-04-30 16:56:10 +0900

    solved/boj/Silver/10844/py

---
+++ Python/boj/Silver/10844.py
@@ -0,0 +1,24 @@
+import sys
+
+if __name__ == "__main__":
+    try:
+        sys.stdin = open("input.txt", "r")
+    except FileNotFoundError:
+        pass
+
+    N = int(input())
+    dp = [[0] * 10 for _ in range(N + 1)]
+
+    for i in range(1, 10):
+        dp[1][i] = 1
+
+    for i in range(2, N + 1):
+        for j in range(0, 10):
+            if j == 0:
+                dp[i][j] = dp[i - 1][1]
+            elif j == 9:
+                dp[i][j] = dp[i - 1][8]
+            else:
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1]
+
+    print(sum(dp[N]) % 1000000000)
