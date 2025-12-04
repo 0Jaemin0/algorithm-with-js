02dfec836d669c1f999e5168a28544c958fabf2f
0Jaemin0 <jaemin2167@naver.com>
2025-12-04 15:41:43 +0900

    solved/boj/Silver/11726/js

---
+++ JavaScript/boj/Silver/11726(2).js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const n = +input[0];
+const dp = Array.from({ length: n + 1 }, () => 0);
+
+dp[1] = 1;
+dp[2] = 2;
+
+for (let i = 3; i <= n; i++) {
+  dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+}
+
+console.log(dp[n]);
