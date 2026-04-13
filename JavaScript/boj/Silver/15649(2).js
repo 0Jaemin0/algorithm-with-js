2b0f8d8e40b959a2be154d2d94c22e5fac122de7
0Jaemin0 <jaemin2167@naver.com>
2026-04-13 19:00:26 +0900

    solved/boj/Silver/15649/js

---
+++ JavaScript/boj/Silver/15649(2).js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const [N, M] = input[0].split(' ').map(Number);
+const numbers = Array.from({ length: N }, (_, i) => i + 1);
+
+const getPermutation = (arr, selectNumber) => {
+  const results = [];
+
+  const backtrack = (current, visited) => {
+    if (current.length === selectNumber) {
+      results.push([...current]);
+
+      return;
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      if (visited[i]) continue;
+
+      visited[i] = true;
+      current.push(arr[i]);
+      backtrack(current, visited);
+      visited[i] = false;
+      current.pop();
+    }
+  };
+
+  backtrack([], new Array(N + 1).fill(false));
+
+  return results;
+};
+
+const permutation = getPermutation(numbers, M);
+
+console.log(permutation.map((list) => list.join(' ')).join('\n'));
